837f2f920cbe9e530a9bc039992c9074e9eb4ae3
Julian <cahill.julian@gmail.com>
2017-04-24 05:48:46 +1200

    move all command code into individual functions in preparation for further tidying up. also added a command selector to select the right functions

--- discordbot.py
+++ discordbot.py
@@ -143,6 +143,198 @@ Debug (Admin) Commands:
 \rename [newname] \setgame [playing] \reactiondel [name] [num] ```'''
 
 
+async def pong(message):
+	await client.send_message(message.channel, '%s pong.' % (message.author.mention))
+
+async def get_random_reddit_image(message):
+	subreddit = message.content.split(' ')[1]
+	dat = requests.get('https://reddit.com/r/' + subreddit + '/.json', headers={'User-Agent':'Discord-Acid-Bot /u/saucecode'}).json()
+
+	urls = [i['data']['url'] for i in dat['data']['children']] # pull urls from reddit post list
+	imgurs = [url for url in urls if any( domain in url for domain in ['imgur.com', 'i.redd.it'] )] # filter links for only these domains
+
+	await client.send_message(message.channel, random.choice(imgurs))
+
+async def get_random_top_reddit_image(message):
+	subreddit = message.content.split(' ')[1]
+	dat = requests.get('https://reddit.com/r/' + subreddit + '/top/.json?t=all', headers={'User-Agent':'Discord-Acid-Bot /u/saucecode'}).json()
+
+	urls = [i['data']['url'] for i in dat['data']['children']] # pull urls from reddit post list
+	imgurs = [url for url in urls if any( domain in url for domain in ['imgur.com', 'i.redd.it'] )] # filter links for only these domains
+
+	await client.send_message(message.channel, random.choice(imgurs) + ' TOP!')
+
+async def do_postfix_calculation(message):
+	await client.send_message(message.channel, postfix.outputResult( postfix.doPostfix(message.content[6:]) ))
+
+async def define_word(message):
+	word = message.content.split(' ')[1]
+	defs = dictionary.get_definitions(word)[:3]
+	if len(defs) == 0:
+		await client.send_message(message.channel, 'What even is a %s?' % word)
+	else:
+		i=1
+		out = ''
+		for s in defs:
+			out += '%i. %s\n' % (i,s)
+			i += 1
+		await client.send_message(message.channel, '```%s```' % (out,))
+
+async def urban_define_word(message):
+	word = message.content[4:]
+	definition = dictionary.get_urban_definitions(word)[0]
+	for i in range(len(definition['definition'])//2000 + 1):
+		await client.send_message(message.channel, '```%s```' % definition['definition'][i*2000:i*2000+2000])
+	await client.send_message(message.channel, '```examples: %s```' % definition['example'])
+
+async def whoami(message):
+	await whois_user(message.channel, message.author)
+
+async def whois(message):
+	target = message.content[7:]
+	member = discord.utils.find(lambda m: target.lower() in m.name.lower() or target.lower() in m.display_name.lower(), message.channel.server.members)
+	if member:
+		await whois_user(message.channel, member)
+	else:
+		await client.send_message(message.channel, 'I can\'t find a %s' % (target,))
+
+async def whois_user(chan, user):
+	await client.send_message(chan, 'Name: %s; Display Name: %s; Discriminator: %s; ID: %s' % (user.name, user.display_name, user.discriminator, user.id))
+
+async def do_imitate(message):
+	name = message.content.split(' ')[1]
+	opts = []
+	opts.extend(message.content.split(' ')[2:])
+
+	max_length = 20
+	try:
+		max_length = int(message.content.split(' ')[2])
+	except:
+		pass
+
+	if name in markov.users:
+		await client.send_message(message.channel, '`%s`' % (markov.imitate(name, max_length=max_length),), tts='tts' in opts)
+
+async def set_game(message):
+	await client.change_presence(game=discord.Game(name=message.content[9:]))
+
+async def rename_bot(message):
+	await client.edit_profile(username=message.content.split(' ')[1])
+	await client.send_message(message.channel, 'Acknowledged.')
+
+async def markov_save(message):
+	markov.save()
+	await client.send_message(message.channel, 'Saved markov.json')
+
+async def markov_load(message):
+	markov.load()
+	await client.send_message(message.channel, 'Loaded markov.json')
+
+async def markov_users(message):
+	await client.send_message(message.channel,
+		', '.join( ['%s (%i)' % (key, len(markov.users[key])) for key in list(markov.users.keys())] )
+	)
+
+async def markov_clear(message):
+	target = message.content[13:]
+	if target in markov.users:
+		del markov.users[target]
+		await client.send_message(message.channel, 'Cleared markov data for %s' % (target,))
+	else:
+		await client.send_message(message.channel, 'Could not find %s in markov.users' % (target,))
+
+async def markov_feed(message):
+	username = message.content.split(' ')[1]
+	url = message.content.split(' ')[2]
+	resp = requests.get(url)
+	if not 'Content-Type' in resp.headers or not 'text/plain' in resp.headers['Content-Type']:
+		await client.send_message(message.channel, 'Not a plaintext file - cannot read!')
+	else:
+		markov.buffer_words(username, remove_urls(resp.text))
+		markov.add_words(username)
+		await client.send_message(message.channel, 'Added to %s with new score: %i' % (username, len(markov.users[username])) )
+
+async def reaction_add(message):
+	name = message.content.split(' ')[1]
+	url = message.content[len(name) + 1 + 13:]
+	if not name in reactions:
+		reactions[name] = []
+
+	reactions[name].append(url)
+
+	with open('reactions.json','w') as f:
+		json.dump(reactions, f)
+
+	await client.send_message(message.channel, '%s has %i responses.' % (name, len(reactions[name])))
+
+async def reaction_do(message):
+	name = message.content[2:]
+	if name[0] == ' ':
+		name = name[1:]
+	if name in reactions and len(reactions[name]) > 0:
+		idx = random.choice( range(len(reactions[name])) )
+		item = reactions[name][idx]
+		await client.send_message(message.channel, '(%i) %s' % (idx,item))
+
+async def reaction_list(message):
+	await client.send_message(message.channel, ', '.join( [x for x in list(reactions.keys()) if len(reactions[x]) > 0] ))
+
+async def reaction_del(message):
+	name = message.content.split(' ')[1]
+	num = int(message.content.split(' ')[2])
+	try:
+		reactions[name].pop(num)
+
+		with open('reactions.json','w') as f:
+			json.dump(reactions, f)
+	except:
+		pass
+
+async def play_5050(message):
+	dat = requests.get('https://reddit.com/r/fiftyfifty/.json', headers={'User-Agent':'Discord-Acid-Bot /u/saucecode'}).json()
+	urls = [(i['data']['title'], i['data']['url']) for i in dat['data']['children'] if 'imgur.com' in i['data']['url'] or 'i.redd.it' in i['data']['url'] or '5050.degstu' in i['data']['url']]
+	titles,url = random.choice(urls)
+	await client.send_message(message.channel, '`%s` or...' % titles.split('|')[0].replace('[50/50] ','') )
+	await client.send_message(message.channel, '`%s`' % titles.split('|')[1] )
+	await asyncio.sleep(7)
+	await client.send_message(message.channel, url)
+
+async def do_help(message):
+	await client.send_message(message.channel, HELP_STRING)
+
+async def flip_coin(message):
+	await client.send_message(message.channel, 'You rolled ' + random.choice( ['Heads', 'Tails.'] ))
+
+commander = {
+	'help':     {'run': do_help},
+	'ping':     {'run': pong},
+	'rr':       {'run': get_random_reddit_image},
+	'calc':     {'run': do_postfix_calculation},
+	'whoami':   {'run': whoami},
+	'whois':    {'run': whois},
+	'define':   {'run': define_word},
+	'ud':       {'run': urban_define_word},
+	'rrtop':    {'run': get_random_top_reddit_image},
+	'imitate':  {'run': do_imitate},
+	'50/50':    {'run': play_5050},
+	'flip':     {'run': flip_coin},
+
+	'markovsave':    {'run': markov_save},
+	'markovload':    {'run': markov_load, 'perms':[181227668241383425]},
+	'markovusers':   {'run': markov_users},
+	'markovclear':   {'run': markov_clear, 'perms':[181227668241383425]},
+	'markovfeed':    {'run': markov_feed, 'perms':[181227668241383425, 304098431973064705]},
+
+	'reactionadd':   {'run': reaction_add},
+	'reactiondel':   {'run': reaction_del, 'perms':[181227668241383425, 304098431973064705]},
+	'reactions':     {'run': reaction_list},
+	'\\':            {'run': reaction_do},
+
+	'setgame':       {'run': set_game, 'perms':[181227668241383425]},
+	'rename':        {'run': rename_bot, 'perms':[181227668241383425]}
+}
+
+
 @client.event
 async def on_ready():
 	print('Logged in as')
@@ -168,161 +360,12 @@ async def on_message(message):
 		markov.last_speaker = message.author.name
 
 
-	if message.content.startswith('\\rr'):
-		subreddit = message.content.split(' ')[1]
-		dat = requests.get('https://reddit.com/r/' + subreddit + '/.json', headers={'User-Agent':'Discord-Acid-Bot /u/saucecode'}).json()
-		urls = [i['data']['url'] for i in dat['data']['children']]
-		imgurs = [i for i in urls if 'imgur.com' in i or 'i.redd.it' in i]
-		await client.send_message(message.channel, random.choice(imgurs))
-
-	elif message.content.startswith('/r'):
-		await client.send_message(message.channel, 'It\'s a backslash, %s.' % sailor_word())
-
-	elif message.content.startswith('\\calc'):
-		await client.send_message(message.channel, postfix.outputResult( postfix.doPostfix(message.content[6:]) ))
-
-	elif message.content.startswith('\\whoami'):
-		#await client.edit_profile(username=message.content.split(' ')[1])
-		await client.send_message(message.channel, 'Name: %s; Display Name: %s; Discriminator: %s; ID: %s' % (message.author.name, message.author.display_name, message.author.discriminator, message.author.id))
-
-	elif message.content.startswith('\\whois'):
-		target = message.content[7:]
-		member = discord.utils.find(lambda m: target.lower() in m.name.lower() or target.lower() in m.display_name.lower(), message.channel.server.members)
-		if member:
-			await client.send_message(message.channel, 'Name: %s; Display Name: %s; Discriminator: %s; ID: %s' % (member.name, member.display_name, member.discriminator, member.id))
-		else:
-			await client.send_message(message.channel, 'I can\'t find a %s' % (target,))
-
-	elif message.content.startswith('\\rename') and int(message.author.id) == 181227668241383425:
-		await client.edit_profile(username=message.content.split(' ')[1])
-		await client.send_message(message.channel, 'Acknowledged.')
-
-	elif message.content.startswith('\\setgame') and int(message.author.id) == 181227668241383425:
-		await client.change_presence(game=discord.Game(name=message.content[9:]))
-
-	elif message.content.startswith('\\ping'):
-		await client.send_message(message.channel, '%s pong.' % (message.author.mention))
-
-	elif message.content.startswith('\\define'):
-		word = message.content.split(' ')[1]
-		defs = dictionary.get_definitions(word)[:3]
-		if len(defs) == 0:
-			await client.send_message(message.channel, 'What even is a %s?' % word)
-		else:
-			i=1
-			out = ''
-			for s in defs:
-				out += '%i. %s\n' % (i,s)
-				i += 1
-			await client.send_message(message.channel, '```%s```' % (out,))
-
-	elif message.content.startswith('\\ud'):
-		word = message.content[4:]
-		definition = dictionary.get_urban_definitions(word)[0]
-		for i in range(len(definition['definition'])//2000 + 1):
-			await client.send_message(message.channel, '```%s```' % definition['definition'][i*2000:i*2000+2000])
-		await client.send_message(message.channel, '```examples: %s```' % definition['example'])
-
-	elif message.content.startswith('\\imitate'):
-		name = message.content.split(' ')[1]
-		opts = []
-		opts.extend(message.content.split(' ')[2:])
-
-		max_length = 20
-		try:
-			max_length = int(message.content.split(' ')[2])
-		except:
-			pass
-
-		if name in markov.users:
-			await client.send_message(message.channel, '`%s`' % (markov.imitate(name, max_length=max_length),), tts='tts' in opts)
-
-	elif message.content.startswith('\\markovsave'):
-		markov.save()
-		await client.send_message(message.channel, 'Saved markov.json')
-
-	elif message.content.startswith('\\markovload') and int(message.author.id) == 181227668241383425:
-		markov.load()
-		await client.send_message(message.channel, 'Loaded markov.json')
-
-	elif message.content.startswith('\\markovusers'):
-		await client.send_message(message.channel,
-			', '.join( ['%s (%i)' % (key, len(markov.users[key])) for key in list(markov.users.keys())] )
-		)
-
-	elif message.content.startswith('\\markovclear') and int(message.author.id) == 181227668241383425:
-		target = message.content[13:]
-		if target in markov.users:
-			del markov.users[target]
-			await client.send_message(message.channel, 'Cleared markov data for %s' % (target,))
-		else:
-			await client.send_message(message.channel, 'Could not find %s in markov.users' % (target,))
-
-	elif message.content.startswith('\\markovfeed') and int(message.author.id) in [181227668241383425, 304098431973064705]:
-		username = message.content.split(' ')[1]
-		url = message.content.split(' ')[2]
-		resp = requests.get(url)
-		if not 'Content-Type' in resp.headers or not 'text/plain' in resp.headers['Content-Type']:
-			await client.send_message(message.channel, 'Not a plaintext file - cannot read!')
-		else:
-			markov.buffer_words(username, remove_urls(resp.text))
-			markov.add_words(username)
-			await client.send_message(message.channel, 'Added to %s with new score: %i' % (username, len(markov.users[username])) )
-
-	elif message.content.startswith('\\50/50'):
-		dat = requests.get('https://reddit.com/r/fiftyfifty/.json', headers={'User-Agent':'Discord-Acid-Bot /u/saucecode'}).json()
-		urls = [(i['data']['title'], i['data']['url']) for i in dat['data']['children'] if 'imgur.com' in i['data']['url'] or 'i.redd.it' in i['data']['url'] or '5050.degstu' in i['data']['url']]
-		titles,url = random.choice(urls)
-		await client.send_message(message.channel, '`%s` or...' % titles.split('|')[0].replace('[50/50] ','') )
-		await client.send_message(message.channel, '`%s`' % titles.split('|')[1] )
-		await asyncio.sleep(7)
-		await client.send_message(message.channel, url)
-
-	elif message.content.startswith('\\help'):
-		await client.send_message(message.channel, HELP_STRING)
-
-	elif message.content.startswith('\\flip'):
-		number = random.randint(1, 2)
-		if number == 1:
-			await client.send_message(message.channel, 'You rolled Heads')
-		elif number == 2:
-			await client.send_message(message.channel, 'You rolled Tails.')
-
-	elif message.content.startswith('\\reactionadd'):
-		name = message.content.split(' ')[1]
-		url = message.content[len(name) + 1 + 13:]
-		if not name in reactions:
-			reactions[name] = []
-
-		reactions[name].append(url)
-
-		with open('reactions.json','w') as f:
-			json.dump(reactions, f)
-
-		await client.send_message(message.channel, '%s has %i responses.' % (name, len(reactions[name])))
-
-	elif message.content.startswith('\\reactiondel') and int(message.author.id) in [181227668241383425, 304098431973064705]:
-		name = message.content.split(' ')[1]
-		num = int(message.content.split(' ')[2])
-		try:
-			reactions[name].pop(num)
-
-			with open('reactions.json','w') as f:
-				json.dump(reactions, f)
-		except:
-			pass
-
-	elif message.content.startswith('\\\\'):
-		name = message.content[2:]
-		if name[0] == ' ':
-			name = name[1:]
-		if name in reactions and len(reactions[name]) > 0:
-			idx = random.choice( range(len(reactions[name])) )
-			item = reactions[name][idx]
-			await client.send_message(message.channel, '(%i) %s' % (idx,item))
-
-	elif message.content.startswith('\\reactions'):
-		await client.send_message(message.channel, ', '.join( [x for x in list(reactions.keys()) if len(reactions[x]) > 0] ))
+	if message.content.startswith('\\') and len(message.content) > 1:
+		command_candidates = sorted([x.group() for x in [re.match(opt.replace('\\', '\\\\'), message.content[1:]) for opt in list(commander.keys())] if x], key=lambda x:len(x), reverse=True)
+		if command_candidates:
+			command = commander[command_candidates[0]]
+			if not 'perms' in command or int(message.author.id) in command['perms']:
+				await command['run'](message)
 
 
 @client.event
