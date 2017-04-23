7c5322b3b9627b1ed477ad8ec48de77e02652c02
Julian <cahill.julian@gmail.com>
2017-04-23 18:02:41 +1200

    added more vulgarity and singletons

--- sailortalk.py
+++ sailortalk.py
@@ -1,9 +1,15 @@
 import random
 
 __firsts =  'shit fuck muffin dumb piss muppet mother goat whore ass pussy cock'.split(' ')
-__seconds = 'cunt bitch tard face fuck stain stick muppet fucker ass face bandit'.split(' ')
+__seconds = 'cunt bitch tard face fuck stain stick muppet fucker ass face bandit eater'.split(' ')
+__singles = [
+	'cuck', 'muppet', 'sex offender', 'cumslut', 'reasonable human being'
+]
 
 def sailor_word():
-	f = random.choice(__firsts)
-	s = random.choice([x for x in __seconds if not x == f])
-	return '%s%s' % (f, s)
+	if random.random() > len(__singles) / ( (len(__firsts) + len(__seconds)) ):
+		f = random.choice(__firsts)
+		s = random.choice([x for x in __seconds if not x == f])
+		return '%s%s' % (f, s)
+	else:
+		return 'you %s' % random.choice(__singles)
